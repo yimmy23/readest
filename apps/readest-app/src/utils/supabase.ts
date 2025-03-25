f22ce1c22551fddb59b44881789ba404312061f9
Huang Xin <chrox.huang@gmail.com>
2025-03-25 12:29:22 +0800

    fix: access runtime variables in function invoke (#703)

--- apps/readest-app/src/utils/supabase.ts
+++ apps/readest-app/src/utils/supabase.ts
@@ -4,7 +4,6 @@ const supabaseUrl =
   process.env['NEXT_PUBLIC_SUPABASE_URL'] || process.env['NEXT_PUBLIC_DEV_SUPABASE_URL']!;
 const supabaseAnonKey =
   process.env['NEXT_PUBLIC_SUPABASE_ANON_KEY'] || process.env['NEXT_PUBLIC_DEV_SUPABASE_ANON_KEY']!;
-const supabaseAdminKey = process.env['SUPABASE_ADMIN_KEY'] || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
@@ -21,6 +20,7 @@ export const createSupabaseClient = (accessToken?: string) => {
 };
 
 export const createSupabaseAdminClient = () => {
+  const supabaseAdminKey = process.env['SUPABASE_ADMIN_KEY'] || '';
   return createClient(supabaseUrl, supabaseAdminKey, {
     auth: {
       persistSession: false,
